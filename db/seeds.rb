b6a645bcd740385529e5efe19cff50e1c1fb27bf
Ryan Anderson <ryan@Ryans-MacBook-Pro.local>
2020-04-13 17:20:05 -0600

    Forced commit to pull origin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,45 @@
 course1 = Course.create(
     name: "Augusta"
 ) 
+
 course2 = Course.create(
     name: "Pinehurst"
 )
+
 course3 = Course.create(
     name: "CityView"
 )
 
 golfer1 = Golfer.create(
-    name: "Glenn Weber"
+    name: "Glenn Weber",
     party_size: 4
 )
 
-golfer2 = Golfer.create (
-    name: "Evan Leather"
+golfer2 = Golfer.create(
+    name: "Evan Leather",
     party_size: 2
 )
 
-golfer3 = Golfer.create (
-    name: "Nick Gill"
+golfer3 = Golfer.create(
+    name: "Nick Gill",
     party_size: 1
+)
+
+teetime1 = Teetime.create(
+    reservation_time: "7:00AM 4/15/2020",
+    golfer: golfer1,
+    course: course1
+)
+
+teetime2 = Teetime.create(
+    reservation_time: "9:00AM 4/15/2020",
+    golfer: golfer2,
+    course: course2
+)
+
+teetime3 = Teetime.create(
+    reservation_time: "9:00AM 4/15/2020",
+    golfer: golfer3,
+    course: course3
 )
 

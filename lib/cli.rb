fe795e317a8f30c92965f5011dce6be4e334f065
Bwoodruff4 <blakewoodruff4@gmail.com>
2020-04-15 16:14:19 -0600

    Added find_reservation and cancel_reservation methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,7 +8,11 @@ class Cli
             if user_input == 'help'
                 help
             elsif user_input == 'make reservation'
-                make_reservation(get_golfer_info)
+                make_reservation
+            elsif user_input == 'find reservation'
+                find_reservation
+            elsif user_input == 'cancel reservation'
+                cancel_reservation
             end
             user_input=gets.chomp
         end
@@ -16,34 +20,41 @@ class Cli
     end
 
     def help
-        puts "/******************* Tee Time Commands **********************/"
-        puts "/'help' -Displays list of commands                           /"
-        puts "/'make reservation' -Allows user to make Tee Time reservation/"
-        puts "/'find reservation' -Finds a reservation by name             /"
-        puts "/'find course' -Displays list of available courses           /"
-        puts "/'change party' -Allows user to change their party size      /"
-        puts "/'exit' -Quits the program                                   /"
+        puts "/********************** Tee Time Commands **********************/"
+        puts "/'help' -Displays list of commands                              /"
+        puts "/'make reservation' -Allows user to make Tee Time reservation   /"
+        puts "/'cancel reseration' -Allows user to cancel Tee Time reservation/"
+        puts "/'find reservation' -Finds a reservation by name                /"
+        puts "/'find courses' -Displays list of available courses              /"
+        puts "/'change party' -Allows user to change their party size         /"
+        puts "/'exit' -Quits the program                                      /"
     end
 
-    def get_golfer_info
+    def get_golfer_name
         puts "Please enter reservation name:"
-        name = gets.chomp.downcase
+        name = gets.chomp
+    end
 
+    def get_golfer_age
         puts "Please enter your age"
         age = Integer(gets) rescue false
         if age == false
             puts "Please enter a valid number"
-            age = Integer(gets) rescue false
+            get_golfer_age
+        else
+            return age
         end
+    end
 
+    def get_golfer_handicap
         puts "Please enter your handicap"
         handicap = Integer(gets) rescue false
         if handicap == false
             puts "Please enter a valid number"
-            handicap = Integer(gets) rescue false
+            get_golfer_handicap
+        else
+            return handicap
         end
-
-        golfer_info = [name, age, handicap]
     end
 
     def get_reservation_time
@@ -90,10 +101,46 @@ class Cli
     end
 
 
-    def make_reservation(golfer_info)
-        golfer = create_new_golfer(golfer_info[0], golfer_info[1], golfer_info[2])
+    def make_reservation
+        golfer = create_new_golfer(get_golfer_name, get_golfer_age, get_golfer_handicap)
         create_new_teetime(get_reservation_time, get_course, golfer, get_party_size)
         puts "Your reservation has been made."
+    end
+
+    def cancel_reservation
+        reservation = find_reservation
+        puts "Is this the reservation you would like to cancel? (y/n)"
+        user_input = gets.chomp.downcase
+
+        if user_input == 'n'
+            return
+        elsif user_input == 'y'
+            reservation.destroy
+            puts "Reservation has been cancelled."
+        end
+    end
+
+    def find_golfer
+        Golfer.find_by(name: get_golfer_name, age: get_golfer_age, handicap: get_golfer_handicap)
+    end
+
+    def find_reservation
+        golfer = find_golfer
+        
+        if golfer == nil
+            puts "Sorry golfer not found."
+            return
+        end
+
+        reservation_time  = get_reservation_time
+        reservation = Teetime.find_by(golfer: golfer, reservation_time: reservation_time)
+
+        if reservation == nil
+            puts "Sorry, reservation not found."
+        else
+            puts "Reservation Time: #{reservation.reservation_time}\nParty Size: #{reservation.party_size}"
+        end
+        reservation
     end
 
 end
